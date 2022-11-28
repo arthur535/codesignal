5cadb54bbae187f8e142e52f5e651cd7275d03cc
Heisenberg <109768662+arthur535@users.noreply.github.com>
2022-11-28 12:43:38 +0400

    Create matrixElementsSum.cpp

---
+++ matrixElementsSum.cpp
@@ -0,0 +1,12 @@
+int solution(vector<vector<int>> matrix) {
+    int sum = 0;
+    for (int i = 0; i < matrix[0].size(); ++i) {
+        for (int j = 0; j < matrix.size(); ++j) {
+            if(matrix[j][i] == 0){
+                break;
+            }
+            sum += matrix[j][i];
+        }
+    }
+    return sum;
+}
