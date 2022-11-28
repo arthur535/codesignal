c461943b6b5ac552d5b9abb9f169c3ad3c0981f8
Heisenberg <109768662+arthur535@users.noreply.github.com>
2022-11-28 12:29:40 +0400

    Create checkPalindrome.js

---
+++ checkPalindrome.js
@@ -0,0 +1,9 @@
+function solution(string) {
+    const len = string.length;
+    for(let i = 0; i < len / 2; i++) {
+        if (string[i] !== string[len - 1 - i] ){
+            return false;
+        }
+    }
+    return true;
+}
